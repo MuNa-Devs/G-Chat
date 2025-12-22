db43aca883c89ea3a3a9298c777231108f17bcd1
Murthy9644 <bvjsmurthy90@gmail.com>
2025-12-22 12:12:22 +0530

    ledhu, solve ipoindi

--- src/pages/dashboard_page/DashBoard.jsx
+++ src/pages/dashboard_page/DashBoard.jsx
@@ -1,18 +1,16 @@
-import react from "react";
-import ReactDOM from "react-dom";
 import styles from './dashboard.module.css';
 
-export default function DashBoard(){    
-    return(
+export default function DashBoard() {
+    return (
         <div className={styles.dashboardMain}>
             <div className={styles.sidebar}></div>
             <div className={styles.body}>
                 <div className={styles.header}>
-                <div className={styles.sidebarButtonContainer}><button><i className={`fa-solid fa-bars ${styles.sidebarButton}`}></i></button></div>
-                <div className={styles.pageNameContainer}><p className={styles.global}>GLOBAL CHAT</p>
-                <p className={styles.smallName}>College Wide Discussions</p></div>
-                <div className={styles.headerInputContainer}>
-                                      
+                    <div className={styles.sidebarButtonContainer}><button><i className={`fa-solid fa-bars ${styles.sidebarButton}`}></i></button></div>
+                    <div className={styles.pageNameContainer}><p className={styles.global}>GLOBAL CHAT</p>
+                        <p className={styles.smallName}>College Wide Discussions</p></div>
+                    <div className={styles.headerInputContainer}>
+
                         <input
                             className={styles.input}
                             placeholder='search...'
@@ -28,9 +26,9 @@ export default function DashBoard(){
                             type='text'
                         /></div>
                         <div className={styles.footerButtons}>
-                        <div className={styles.smiley}><i className={`fa-regular fa-face-smile ${styles.smile}`}></i></div>
-                        <div className={styles.attach}><i className={`fa-solid fa-paperclip ${styles.smile}`}></i></div>
-                        <button className={styles.sendButton}>Send</button>
+                            <div className={styles.smiley}><i className={`fa-regular fa-face-smile ${styles.smile}`}></i></div>
+                            <div className={styles.attach}><i className={`fa-solid fa-paperclip ${styles.smile}`}></i></div>
+                            <button className={styles.sendButton}>Send</button>
                         </div>
                     </div>
                 </div>
