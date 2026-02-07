bdd657a8efeaad6f20607de36fbbfae902aa1df0
B. V. J. S. Murthy <bvjsmurthy90@gmail.com>
2026-02-07 16:51:13 +0530

    added user profile

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import LoadingScreen from "./pages/loading_screen/LoadingScreen";
 import Settings from "./pages/settings_page/Settings";
 import UserSettings from "./pages/user_settings/UserSettings";
 import DM from "./pages/direct-messages/DM";
-import UserProfile from "./pages/user_profile/UserProfile";
 
 export default function App() {
 	return (
@@ -84,14 +83,6 @@ export default function App() {
 				path="/user-settings" element={
 					<LoginProtector>
 						<UserSettings />
-					</LoginProtector>
-				}
-			/>
-
-			<Route
-				path="/user/profile:user_id" element={
-					<LoginProtector>
-						<UserProfile />
 					</LoginProtector>
 				}
 			/>
--- src/pages/user_profile/UserProfile.jsx
+++ src/pages/user_profile/UserProfile.jsx
@@ -1,40 +1,61 @@
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 import styles from "./user_profile.module.css";
 import { server_url } from "../../../creds/server_url";
 
-export default function UserProfile(props){
-    const {user_id} = useParams();
+export default function UserProfile(props) {
+    const user_id = props.user_id;
     const [user_data, setUserData] = useState({});
 
     useEffect(() => {
         axios.get(server_url + `/g-chat/users/get-user?user_id=${user_id}`)
-        .then(res => {
-            setUserData(res.data.user_details);
-        }).catch(err => {
-            console.log(err);
-        })
-    }, [])
+            .then(res => {
+                setUserData(res.data.user_details);
+            }).catch(err => {
+                console.log(err);
+            })
+    }, []);
 
     return (
-        <div className={styles.profileMainDiv}>
-            <div className={styles.userProfileInfo}>
-                <div className={styles.pfp}>
-                    <img
-                        src={server_url + "/files/" + user_data?.pfp}
-                        onError={(e) => {
-                            e.target.onerror = null;
-                            e.target.src = "https://cdn-icons-png.flaticon.com/512/4847/4847985.png";
-                        }}
-                    />
+        <div className={styles.wrapper}>
+            <div className={styles.profileMainDiv}>
+                <div className={styles.userProfileInfo}>
+                    <div className={styles.pfp}>
+                        <img
+                            src={server_url + "/files/" + user_data?.pfp}
+                            onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = "https://cdn-icons-png.flaticon.com/512/4847/4847985.png";
+                            }}
+                        />
+                    </div>
+
+                    <div className={styles.profileName}>
+                        <h2>{user_data?.username || "User"}</h2>
+
+                        <h5>{user_data?.department}</h5>
+                    </div>
                 </div>
 
-                <div className={styles.profileName}>
-                    <h2>{user_data?.username || "User"}</h2>
-                    
-                    <h5>{user_data?.department || ""}</h5>
+                <hr />
+
+                {
+                    user_data?.about
+                    &&
+                    <>
+                        <div className={styles.about}>
+                            <h3>About</h3>
+
+                            <h5>{user_data?.about + " hahaha hah ahhhahahah ahhaha hahahahah ahahhahaha hahaha haha"}</h5>
+                        </div>
+
+                        <hr />
+                    </>
+                }
+
+                <div className={styles.bottomBtns}>
+                    <button><i className="fa-regular fa-message"></i> Message</button>
                 </div>
             </div>
         </div>
