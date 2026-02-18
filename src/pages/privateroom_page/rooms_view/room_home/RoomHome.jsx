ac2f433d53fdf4127b17a064f23cdf45fd38b9db
B. V. J. S. Murthy <bvjsmurthy90@gmail.com>
2026-02-18 20:32:35 +0530

    user profile ui complete

--- src/pages/privateroom_page/rooms_view/room_home/RoomHome.jsx
+++ src/pages/privateroom_page/rooms_view/room_home/RoomHome.jsx
@@ -10,8 +10,12 @@ import { File, Message } from "../../../../reusable_component/message_dev/Messag
 import EmojiBox from "../../../../reusable_component/emoji_box/EmojiBox";
 import FileObject from "../../../../reusable_component/file_object/FileObject";
 import MessageBar from "../../../../reusable_component/message_bar/MessageBar";
+import PageLoader from "../../../loading_screen/PageLoader";
 
 export default function RoomHome() {
+    // To set the loader screen until the data loads
+    const [loader, setLoader] = useState(true);
+
     // Ref to auto scroll to bottom of the messages
     const bottom_ref = useRef(null);
 
@@ -156,7 +160,7 @@ export default function RoomHome() {
                 sender_pfp: user_details?.pfp
             },
             text: message.trim(),
-            files_list: files.map(file => ({filename: file.name, file_url: null})),
+            files_list: files.map(file => ({ filename: file.name, file_url: null })),
             timestamp: null,
             status: "pending"
         };
@@ -261,6 +265,12 @@ export default function RoomHome() {
         }
     }, [socket]);
 
+    useEffect(() => {
+        if (!Object.keys(room_data).length) return;
+
+        setLoader(false);
+    }, [room_data]);
+
     return (
         <div className={styles.chatpage}>
             {
@@ -272,143 +282,149 @@ export default function RoomHome() {
                 />
             }
 
-            <div className={styles.chatContext}>
-                <div className={styles.chatWindow}>
-                    <div
-                        className={styles.roomControlBar}
-                        onClick={() => {
-                            if (window.innerWidth > 490) return;
+            {
+                loader
+                ?
+                <PageLoader />
+                :
+                <div className={styles.chatContext}>
+                    <div className={styles.chatWindow}>
+                        <div
+                            className={styles.roomControlBar}
+                            onClick={() => {
+                                if (window.innerWidth > 490) return;
 
-                            navigate(
-                                `/room/dashboard/${room_id}`,
-                                {
-                                    state: {
-                                        from: `/room/home/${room_id}`
+                                navigate(
+                                    `/room/dashboard/${room_id}`,
+                                    {
+                                        state: {
+                                            from: `/room/home/${room_id}`
+                                        }
                                     }
-                                }
-                            );
-                        }}
-                    >
-                        <button
-                            className={styles.backBtn}
-                            onClick={() => navigate("/rooms")}
+                                );
+                            }}
                         >
-                            <i className="fa-solid fa-chevron-left"></i>
-                        </button>
+                            <button
+                                className={styles.backBtn}
+                                onClick={() => navigate("/rooms")}
+                            >
+                                <i className="fa-solid fa-chevron-left"></i>
+                            </button>
 
-                        <div className={styles.room_info}>
-                            <img
-                                src={server_url + `/files/${room_data.icon_url}`}
-                                onError={(e) => {
-                                    e.target.onError = null;
-                                    e.target.src = "https://cdn-icons-png.flaticon.com/512/8184/8184182.png";
-                                }}
-                            />
+                            <div className={styles.room_info}>
+                                <img
+                                    src={server_url + `/files/${room_data.icon_url}`}
+                                    onError={(e) => {
+                                        e.target.onError = null;
+                                        e.target.src = "https://cdn-icons-png.flaticon.com/512/8184/8184182.png";
+                                    }}
+                                />
 
-                            <h2>{room_data.r_name}</h2>
+                                <h2>{room_data.r_name}</h2>
+                            </div>
+
+                            <button
+                                className={styles.roomOptions}
+                                onClick={() => navigate(
+                                    `/room/dashboard/${room_id}`,
+                                    {
+                                        state: {
+                                            from: `/room/home/${room_id}`
+                                        }
+                                    }
+                                )}
+                            ><i className="fa-solid fa-bars"></i></button>
                         </div>
 
-                        <button
-                            className={styles.roomOptions}
-                            onClick={() => navigate(
-                                `/room/dashboard/${room_id}`,
-                                {
-                                    state: {
-                                        from: `/room/home/${room_id}`
-                                    }
-                                }
-                            )}
-                        ><i className="fa-solid fa-bars"></i></button>
-                    </div>
+                        <div className={styles.chatContainer}>
+                            {
+                                messages.length ?
+                                    messages.map((msg, index) => (
+                                        <div key={msg.msg_id || msg.identifiers.message_id}>
+                                            {
+                                                msg.files_list.map((file, file_index) => (
+                                                    <File
+                                                        key={`${msg.msg_id || msg.identifiers.message_id}-${file.filename}`}
+                                                        conseqFiles={messages[index - 1]?.sender_details.sender_id === msg.sender_details.sender_id}
+                                                        sender_id={msg.sender_details.sender_id}
+                                                        sender_name={msg.sender_details.sender_name}
+                                                        sender_pfp={msg.sender_details.sender_pfp}
+                                                        filename={file.filename}
+                                                        file_url={file.file_url}
+                                                        timestamp={msg.timestamp}
+                                                        status={msg.status || "complete"}
+                                                    />
+                                                ))
+                                            }
 
-                    <div className={styles.chatContainer}>
-                        {
-                            messages.length ?
-                                messages.map((msg, index) => (
-                                    <div key={msg.msg_id || msg.identifiers.message_id}>
-                                        {
-                                            msg.files_list.map((file, file_index) => (
-                                                <File
-                                                    key={`${msg.msg_id || msg.identifiers.message_id}-${file.filename}`}
-                                                    conseqFiles={messages[index - 1]?.sender_details.sender_id === msg.sender_details.sender_id}
+                                            {
+                                                msg.text
+                                                &&
+                                                <Message
+                                                    key={msg.msg_id || msg.identifiers.message_id}
+                                                    conseq_msgs={messages[index - 1]?.sender_details.sender_id === msg.sender_details.sender_id}
+                                                    message={msg.text}
                                                     sender_id={msg.sender_details.sender_id}
                                                     sender_name={msg.sender_details.sender_name}
                                                     sender_pfp={msg.sender_details.sender_pfp}
-                                                    filename={file.filename}
-                                                    file_url={file.file_url}
                                                     timestamp={msg.timestamp}
+                                                    files={msg.files_list}
                                                     status={msg.status || "complete"}
                                                 />
-                                            ))
-                                        }
+                                            }
+                                        </div>
+                                    )) :
+                                    <div className={styles.noMsgs}><h5>No Recent Messages</h5></div>
+                            }
 
-                                        {
-                                            msg.text
-                                            &&
-                                            <Message
-                                                key={msg.msg_id || msg.identifiers.message_id}
-                                                conseq_msgs={messages[index - 1]?.sender_details.sender_id === msg.sender_details.sender_id}
-                                                message={msg.text}
-                                                sender_id={msg.sender_details.sender_id}
-                                                sender_name={msg.sender_details.sender_name}
-                                                sender_pfp={msg.sender_details.sender_pfp}
-                                                timestamp={msg.timestamp}
-                                                files={msg.files_list}
-                                                status={msg.status || "complete"}
-                                            />
-                                        }
-                                    </div>
-                                )) :
-                                <div className={styles.noMsgs}><h5>No Recent Messages</h5></div>
+                            <div ref={bottom_ref}></div>
+                        </div>
+
+                        {
+                            show_file_object
+                            &&
+                            <FileObject
+                                files={files}
+                            />
                         }
 
-                        <div ref={bottom_ref}></div>
-                    </div>
-
-                    {
-                        show_file_object
-                        &&
-                        <FileObject
-                            files={files}
+                        <MessageBar
+                            setShowPicker={setShowPicker}
+                            handleFiles={handleFiles}
+                            input_ref={input_ref}
+                            message={message}
+                            setMessage={setMessage}
+                            sendMessage={sendMessage}
                         />
-                    }
 
-                    <MessageBar
-                        setShowPicker={setShowPicker}
-                        handleFiles={handleFiles}
-                        input_ref={input_ref}
-                        message={message}
-                        setMessage={setMessage}
-                        sendMessage={sendMessage}
-                    />
+                        {
+                            show_picker
+                            &&
+                            <div className={styles.emojiPicker}>
+                                <EmojiBox
+                                    setEmoji={setEmoji}
+                                />
+                            </div>
+                        }
+                    </div>
 
-                    {
-                        show_picker
-                        &&
-                        <div className={styles.emojiPicker}>
-                            <EmojiBox
-                                setEmoji={setEmoji}
-                            />
+                    <div className={styles.roomContents}>
+                        <h3>Room Contents</h3>
+
+                        <div className={styles.pinnedMsgs}>
+                            <h4>Pinned Messages</h4>
+
+                            <h5>No Pinned Messages</h5>
                         </div>
-                    }
-                </div>
 
-                <div className={styles.roomContents}>
-                    <h3>Room Contents</h3>
+                        <div className={styles.materials}>
+                            <h4>Materials</h4>
 
-                    <div className={styles.pinnedMsgs}>
-                        <h4>Pinned Messages</h4>
-
-                        <h5>No Pinned Messages</h5>
-                    </div>
-
-                    <div className={styles.materials}>
-                        <h4>Materials</h4>
-
-                        <h5>No Materials Uploaded</h5>
+                            <h5>No Materials Uploaded</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
--- src/pages/user_profile/UserProfile.jsx
+++ src/pages/user_profile/UserProfile.jsx
@@ -4,65 +4,140 @@ import axios from "axios";
 import styles from "./user_profile.module.css";
 import { server_url } from "../../../creds/server_url";
 import { AppContext } from "../../Contexts";
+import PageLoader from "../loading_screen/PageLoader";
 
 export default function UserProfile(props) {
+    const [loader, setLoader] = useState(true);
+
     const user_id = props.user_id;
     const { user_details } = useContext(AppContext);
     const [user_data, setUserData] = useState({});
 
     useEffect(() => {
-        axios.get(server_url + `/g-chat/users/get-user?user_id=${user_details?.id || localStorage.getItem("user_id")}&req_user_id=${user_id}`)
+        axios.get(
+            server_url + `/g-chat/users/get-user?user_id=${user_details?.id || localStorage.getItem("user_id")}&req_user_id=${user_id}`,
+            {
+                headers: {
+                    auth_token: `Bearer ${localStorage.getItem("token")}`
+                }
+            }
+        )
             .then(res => {
-                setUserData(res.data.user_details);
+                setUserData(res.data.user);
             }).catch(err => {
                 console.log(err);
-                
+
                 if (["INVALID_JWT", "FORBIDDEN"].includes(err.response?.data?.code))
                     setLogOut();
             })
     }, []);
 
+    useEffect(() => {
+        if (!Object.keys(user_data).length)
+            return;
+
+        setLoader(false);
+    }, [user_data]);
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.profileMainDiv}>
-                <div className={styles.userProfileInfo}>
-                    <div className={styles.pfp}>
-                        <img
-                            src={server_url + "/files/" + user_data?.pfp}
-                            onError={(e) => {
-                                e.target.onerror = null;
-                                e.target.src = "https://cdn-icons-png.flaticon.com/512/4847/4847985.png";
-                            }}
-                        />
-                    </div>
-
-                    <div className={styles.profileName}>
-                        <h2>{user_data?.username || "User"}</h2>
-
-                        <h5>{user_data?.department}</h5>
-                    </div>
-                </div>
-
-                <hr />
-
                 {
-                    user_data?.about
-                    &&
-                    <>
-                        <div className={styles.about}>
-                            <h3>About</h3>
+                    loader
+                        ?
+                        <PageLoader />
+                        :
+                        <>
+                            <div className={styles.userProfileInfo}
+                            >
+                                <div className={styles.pfp}>
+                                    <img
+                                        src={server_url + "/files/" + user_data?.pfp}
+                                        onError={(e) => {
+                                            e.target.onerror = null;
+                                            e.target.src = "https://cdn-icons-png.flaticon.com/512/4847/4847985.png";
+                                        }}
+                                    />
+                                </div>
 
-                            <h5>{user_data?.about}</h5>
-                        </div>
+                                <div className={styles.profileName}>
+                                    <h2>{user_data?.username + (user_data.id === user_details?.id ? " (You)" : "") || "User"}</h2>
 
-                        <hr />
-                    </>
+                                    {
+                                        user_data.department
+                                            ? <h5>{user_data?.department}</h5>
+                                            : <p style={{
+                                                fontStyle: "italic",
+                                                color: "var(--text-secondary)",
+                                                fontSize: ".7em"
+                                            }}>Department info not shared</p>
+                                    }
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div className={styles.about}>
+                                <h3>About</h3>
+
+                                {
+                                    user_data.about
+                                        ?
+                                        <h5>{user_data?.about}</h5>
+                                        :
+                                        <p style={{
+                                            fontStyle: "italic",
+                                            color: "var(--text-secondary)",
+                                            fontSize: ".75em"
+                                        }}>No bio added yet</p>
+                                }
+                            </div>
+
+                            <hr />
+
+                            {
+                                user_data.id !== user_details?.id
+                                &&
+                                <div className={styles.bottomBtns}>
+                                    {
+                                        user_data.is_friend
+                                            ?
+                                            <>
+                                                <button><i className="fa-regular fa-message"></i> Message</button>
+                                                <button
+                                                    style={{
+                                                        backgroundColor: "transparent",
+                                                        border: "none",
+                                                        outline: "none",
+                                                        color: "var(--danger)",
+                                                        fontWeight: "500",
+                                                        letterSpacing: "1px"
+                                                    }}
+                                                ><i style={{ color: "var(--danger)" }} className="fa-solid fa-user-minus"></i> Remove Friend</button>
+                                            </>
+                                            :
+                                            <>
+                                                <button> <i className="fa-solid fa-user-plus"></i> Add Friend</button>
+                                            </>
+                                    }
+                                </div>
+                            }
+
+                            <button
+                                style={{
+                                    backgroundColor: "transparent",
+                                    border: "none",
+                                    outline: "none",
+                                    color: "var(--danger)",
+                                    fontWeight: "500",
+                                    letterSpacing: "1px",
+                                    marginTop: "12px"
+                                }}
+                                onClick={() => props.closeHook(false)}
+                            >Close</button>
+                        </>
                 }
-
-                <div className={styles.bottomBtns}>
-                    <button><i className="fa-regular fa-message"></i> Message</button>
-                </div>
             </div>
-        </div>
+        </div >
     );
 }
