a74b5b1ff786f471e4310e37a42312be1ac9bfe2
naidu2005 <kumasomeswari@gmail.com>
2026-03-21 16:00:05 +0530

    git bug reslove

--- src/pages/assignments_page/components/browse_writers/BrowseWriters.jsx
+++ src/pages/assignments_page/components/browse_writers/BrowseWriters.jsx
@@ -2,28 +2,50 @@ import { useEffect, useState } from "react";
 import styles from "./browse_writers.module.css";
 import WriterCard from "../../reusable_component/writer_card/WriterCard";
 import axios from "axios";
+import { server_url } from "../../../../../creds/server_url";
 
 export default function BrowseWriters() {
     const [writers, setWriters] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-axios.get(`${server_url}/g-chat/orders/writer/all`, {
-    headers: {
-        auth_token: `Bearer ${localStorage.getItem("token")}`
-    }
-})
+        axios.get(
+            `${server_url}/g-chat/writers/all`,
+            {
+                headers: {
+                    auth_token: `Bearer ${localStorage.getItem("token")}`
+                }
+            }
+        )
+        .then((res) => {
+            setWriters(res.data.writers);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+    }, []);
+
+    const filteredWriters = writers.filter((writer) =>
+        writer.username
+            .toLowerCase()
+            .includes(search.toLowerCase())
+    );
 
     return (
         <div className={styles.page}>
 
             <div className={styles.searchBar}>
                 <input
+                    type="text"
                     placeholder="Search writers..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </div>
 
             <div className={styles.grid}>
-                {writers.map((writer) => (
+                {filteredWriters.map((writer) => (
                     <WriterCard
                         key={writer.writer_id}
                         writer={writer}
